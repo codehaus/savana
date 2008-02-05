fda83c810a5f37a526dcc378167694e506ef81f7
bryon <bryon@7c000575-7f3e-0410-8e6b-90f0b5f38242>
2008-02-05 20:25:09 +0000

    removed 'ss' from several messages/docs, refactored sav and sav.bat

---
+++ src/main/java/org/codehaus/savana/util/cli/CommandAliases.java
@@ -0,0 +1,65 @@
+package org.codehaus.savana.util.cli;
+
+import java.io.IOException;
+import java.util.*;
+
+/**
+ * Savana - Transactional Workspaces for Subversion
+ * Copyright (C) 2006  Bazaarvoice Inc.
+ * <p/>
+ * This file is part of Savana.
+ * <p/>
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * <p/>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * <p/>
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * @author Bryon Jacob (bryon@jacob.net)
+ */
+public class CommandAliases {
+    private final Map<String, String> aliasMap = new HashMap<String, String>();
+    private final Properties props = new Properties();
+
+    public CommandAliases() {
+        try {
+            props.load(CommandAliases.class.getClassLoader().getResourceAsStream("commands.properties"));
+        } catch (IOException e) {
+            throw new RuntimeException(e); // TODO: create a "fatal exception" to throw here.
+        }
+        for (Map.Entry entry : props.entrySet()) {
+            String className = String.valueOf(entry.getKey());
+            String[] aliases = String.valueOf(entry.getValue()).split(",");
+            for (String alias : aliases) {
+                aliasMap.put(alias, className);
+            }
+        }
+    }
+
+    public Class loadCommandClassByNameOrAlias(String name) throws ClassNotFoundException {
+        String className = aliasMap.get(name);
+        return Class.forName(className == null ? name : className);
+    }
+
+    public List<String> getCommandList() {
+        List<String> commandList = new ArrayList<String>();
+        for (Object val : props.values()) {
+            String[] vals = String.valueOf(val).split(",", 2);
+            commandList.add(vals[0] +
+                            ((vals.length > 1) ?
+                             ("(" + vals[1] + ")") :
+                             ""));
+        }
+        Collections.sort(commandList);
+        return commandList;
+    }
+
+}
