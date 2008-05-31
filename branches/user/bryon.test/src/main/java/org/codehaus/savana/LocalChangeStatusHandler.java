352e08da8f8172f3980bb13956a348715186060c
bryon <bryon@7c000575-7f3e-0410-8e6b-90f0b5f38242>
2008-05-31 23:03:44 +0000

    Creating branch: bryon.test

---
+++ branches/user/bryon.test/src/main/java/org/codehaus/savana/LocalChangeStatusHandler.java
@@ -0,0 +1,85 @@
+package org.codehaus.savana;
+
+import org.tmatesoft.svn.core.SVNCancelException;
+import org.tmatesoft.svn.core.wc.*;
+
+/**
+ * Savana - Transactional Workspaces for Subversion
+ * Copyright (C) 2006  Bazaarvoice Inc.
+ * <p/>
+ * This file is part of Savana.
+ * <p/>
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * <p/>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * <p/>
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Third party components of this software are provided or made available only subject
+ * to their respective licenses. The relevant components and corresponding
+ * licenses are listed in the "licenses" directory in this distribution. In any event,
+ * the disclaimer of warranty and limitation of liability provision in this Agreement
+ * will apply to all Software in this distribution.
+ *
+ * @author Brian Showers (brian@bazaarvoice.com)
+ * @author Bryon Jacob (bryon@jacob.net)
+ */
+public class LocalChangeStatusHandler implements ISVNStatusHandler, ISVNEventHandler {
+    private boolean _changed;
+
+    public LocalChangeStatusHandler() {
+        _changed = false;
+    }
+
+    public boolean isChanged() {
+        return _changed;
+    }
+
+    public void handleStatus(SVNStatus status) {
+
+        //Check the status of the file
+        SVNStatusType contentsStatus = status.getContentsStatus();
+        if (contentsStatus == SVNStatusType.STATUS_MODIFIED ||
+            contentsStatus == SVNStatusType.STATUS_CONFLICTED ||
+            contentsStatus == SVNStatusType.STATUS_MERGED ||
+            contentsStatus == SVNStatusType.STATUS_DELETED ||
+            contentsStatus == SVNStatusType.STATUS_ADDED ||
+            contentsStatus == SVNStatusType.STATUS_MISSING ||
+            contentsStatus == SVNStatusType.STATUS_INCOMPLETE ||
+            contentsStatus == SVNStatusType.STATUS_OBSTRUCTED ||
+            contentsStatus == SVNStatusType.STATUS_REPLACED) {
+
+            _changed = true;
+        }
+
+        //Check the status of the file's properties
+        SVNStatusType propertiesStatus = status.getPropertiesStatus();
+        if (propertiesStatus == SVNStatusType.STATUS_MODIFIED ||
+            propertiesStatus == SVNStatusType.STATUS_CONFLICTED) {
+
+            _changed = true;
+        }
+    }
+
+    /*
+     * This is an implementation for
+     * ISVNEventHandler.handleEvent(SVNEvent event, double progress)
+     */
+    public void handleEvent(SVNEvent event, double progress) {
+    }
+
+    /*
+     * Should be implemented to check if the current operation is cancelled. If
+     * it is, this method should throw an SVNCancelException.
+     */
+    public void checkCancelled() throws SVNCancelException {
+    }
+}
---
+++ branches/user/bryon.test/src/main/java/org/codehaus/savana/scripts/ListChangesFromSource.java
@@ -0,0 +1,101 @@
+package org.codehaus.savana.scripts;
+
+import org.codehaus.savana.FileListDiffGenerator;
+import org.codehaus.savana.SVNScriptException;
+import org.codehaus.savana.WorkingCopyInfo;
+import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.SVNURL;
+import org.tmatesoft.svn.core.wc.SVNDiffClient;
+import org.tmatesoft.svn.core.wc.SVNRevision;
+
+import java.util.Set;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Savana - Transactional Workspaces for Subversion
+ * Copyright (C) 2006  Bazaarvoice Inc.
+ * <p/>
+ * This file is part of Savana.
+ * <p/>
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * <p/>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * <p/>
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Third party components of this software are provided or made available only subject
+ * to their respective licenses. The relevant components and corresponding
+ * licenses are listed in the "licenses" directory in this distribution. In any event,
+ * the disclaimer of warranty and limitation of liability provision in this Agreement
+ * will apply to all Software in this distribution.
+ *
+ * @author Brian Showers (brian@bazaarvoice.com)
+ * @author Bryon Jacob (bryon@jacob.net)
+ */
+public class ListChangesFromSource extends SVNScript {
+    public ListChangesFromSource()
+            throws SVNException, SVNScriptException {}
+
+    public void run()
+            throws SVNException, SVNScriptException {
+        WorkingCopyInfo wcInfo = new WorkingCopyInfo(_clientManager);
+
+        //If there is no source (we are in the trunk)
+        if (wcInfo.getSourcePath() == null) {
+            String errorMessage = "Error: No source path found (you are probably in the TRUNK).";
+            throw new SVNScriptException(errorMessage);
+        }
+
+        //Create the source URL
+        SVNURL repositoryURL = getRepositoryURL();
+        SVNURL sourceURL = repositoryURL.appendPath(wcInfo.getSourcePath(), false);
+
+        //Diff [source:HEAD, branch:HEAD] to see what has changes
+        logStart("Get Diff Client");
+        SVNDiffClient diffClient = _clientManager.getDiffClient();
+        FileListDiffGenerator diffGenerator = new FileListDiffGenerator();
+        diffClient.setDiffGenerator(diffGenerator);
+        logEnd("Get Diff Client");
+        logStart("Do Diff");
+        diffClient.doDiff(sourceURL, wcInfo.getLastMergeRevision(), wcInfo.getRootDir(), SVNRevision.WORKING, true, false, getOut());
+        logEnd("Do Diff");
+
+        String workingCopyPath = wcInfo.getRootDir().getAbsolutePath();
+        printFileList("Added Files:", diffGenerator.getAddedFilePaths(), workingCopyPath);
+        printFileList("Modified Files:", diffGenerator.getChangedFilePaths(), workingCopyPath);
+        printFileList("Deleted Files:", diffGenerator.getDeletedFilePaths(), workingCopyPath);
+    }
+
+    private void printFileList(String title, Set<String> paths, String workingCopyPath)
+            throws SVNScriptException {
+        try {
+            workingCopyPath = new File(workingCopyPath).getCanonicalPath();
+            if (!paths.isEmpty()) {
+                getOut().println(title);
+                getOut().println("-------------------------------------------------");
+                for (String path : paths) {
+                    path = new File(path).getCanonicalPath();
+                    if (!workingCopyPath.equals(path)) {
+                        getOut().println(path.substring(workingCopyPath.length() + 1));
+                    }
+                }
+                getOut().println("");
+            }
+        } catch (IOException e) {
+            throw new SVNScriptException(e);
+        }
+    }
+
+    public String getUsageMessage() {
+        return _commandLineProcessor.usage("listchanegsfromsource");
+    }
+}
