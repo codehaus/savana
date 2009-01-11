d9a02f1de8d110985f98dbdbefcd0a4a65a7073a
ssmith <ssmith@7c000575-7f3e-0410-8e6b-90f0b5f38242>
2009-01-11 02:35:41 +0000

    shawn-subbranch - create branch

---
+++ shawn-subbranch/src/main/java/org/codehaus/savana/BranchType.java
@@ -0,0 +1,76 @@
+/*
+ * Savana - Transactional Workspaces for Subversion
+ * Copyright (C) 2009  Bazaarvoice Inc.
+ * <p/>
+ * This file is part of Savana.
+ * <p/>
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * <p/>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * <p/>
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Third party components of this software are provided or made available only subject
+ * to their respective licenses. The relevant components and corresponding
+ * licenses are listed in the "licenses" directory in this distribution. In any event,
+ * the disclaimer of warranty and limitation of liability provision in this Agreement
+ * will apply to all Software in this distribution.
+ *
+ * @author Brian Showers (brian@bazaarvoice.com)
+ * @author Bryon Jacob (bryon@jacob.net)
+ * @author Shawn Smith (shawn@bazaarvoice.com)
+ */
+package org.codehaus.savana;
+
+public enum BranchType {
+
+    /**
+     * The trunk branch is the main branch for ongoing development work.  There is one of these per project.
+     */
+    TRUNK("TRUNK", "trunk"),
+
+    /**
+     * A release branch is a branch off the trunk corresponding to a particular major version of the project.
+     * Work on patches to the release will occur off the release branch.
+     */
+    RELEASE_BRANCH("RELEASE BRANCH", "branches"),
+
+    /**
+     * A user branch is a private workspace where changes to the trunk or to a release branch can be developed
+     * before being promoted to a trunk or release branch. 
+     */
+    USER_BRANCH("USER BRANCH", "branches/user");
+
+    private final String _keyword;
+    private final String _defaultPath;
+
+    private BranchType(String keyword, String defaultPath) {
+        _keyword = keyword;
+        _defaultPath = defaultPath;
+    }
+
+    public String getKeyword() {
+        return _keyword;
+    }
+
+    public String getDefaultPath() {
+        return _defaultPath;
+    }
+
+    public static BranchType fromKeyword(String keyword) {
+        for (BranchType branchType : BranchType.values()) {
+            if (branchType.name().equalsIgnoreCase(keyword) || branchType.getKeyword().equalsIgnoreCase(keyword)) {
+                return branchType;
+            }
+        }
+        throw new IllegalArgumentException("Unknown branch type: " + keyword);
+    }
+}
---
+++ shawn-subbranch/src/test/java/org/codehaus/savana/scripts/TestSvnUtil.java
@@ -0,0 +1,84 @@
+package org.codehaus.savana.scripts;
+
+import org.apache.commons.lang.StringUtils;
+import org.tmatesoft.svn.core.internal.wc.admin.SVNAdminAreaFactory;
+
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Utilities for working with the Subversion command-line programs (not SVNKit).
+ */
+public class TestSvnUtil {
+    private static final Logger _sLog = Logger.getLogger(TestRepoUtil.class.getName());
+
+    public static final int WC_FORMAT;
+
+    public static final boolean REPO_PRE14;
+    public static final boolean REPO_PRE15;
+
+    static {
+        // detect the version of the 'svn' command-line utility so the tests can create
+        // working copy files that are compatible with 'svn'.
+        String clientVersionString;
+        try {
+            clientVersionString = TestProcessUtil.exec("svn", "--version", "--quiet").trim();
+        } catch (RuntimeException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+
+        String[] clientFields = StringUtils.split(clientVersionString, ".");
+        int clientMajor = Integer.parseInt(clientFields[0]);
+        int clientMinor = Integer.parseInt(clientFields[1]);
+
+        if (clientMajor == 1 && clientMinor == 3) {
+            _sLog.info("Using svn 1.3-compatible working copy file format");
+            WC_FORMAT = SVNAdminAreaFactory.WC_FORMAT_13;
+
+        } else if (clientMajor == 1 && clientMinor == 4) {
+            _sLog.info("Using svn 1.4-compatible working copy file format");
+            WC_FORMAT = SVNAdminAreaFactory.WC_FORMAT_14;
+
+        } else {
+            _sLog.info("Using svn 1.5-compatible working copy file format");
+            WC_FORMAT = SVNAdminAreaFactory.WC_FORMAT_15;
+        }
+
+        // detect the version of the 'svnlook' command-line utility used by the subversion
+        // server.  the back-end pre-commit hook uses svnlook, so the test repository file
+        // format needs to be compatible with it.
+        String serverVersionString;
+        try {
+            serverVersionString = TestProcessUtil.exec("svnlook", "--version").trim();
+        } catch (RuntimeException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+        Matcher serverMatcher = Pattern.compile("version ([0-9]+)\\.([0-9]+)\\b").matcher(serverVersionString);
+        if (!serverMatcher.find()) {
+            throw new IllegalStateException("Unexpected svnlook version string: " + serverVersionString);
+        }
+        int serverMajor = Integer.parseInt(serverMatcher.group(1));
+        int serverMinor = Integer.parseInt(serverMatcher.group(2));
+
+        if (serverMajor == 1 && serverMinor == 3) {
+            _sLog.info("Using svn 1.3-compatible repository format");
+            REPO_PRE14 = true;
+            REPO_PRE15 = true;
+
+        } else if (serverMajor == 1 && serverMinor == 4) {
+            _sLog.info("Using svn 1.4-compatible repository format");
+            REPO_PRE14 = false;
+            REPO_PRE15 = true;
+
+        } else {
+            _sLog.info("Using svn 1.5-compatible repository format");
+            REPO_PRE14 = false;
+            REPO_PRE15 = false;
+        }
+    }
+}
