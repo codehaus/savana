585d6d300d4c556c47e7c881881f97c0f1336e46
ssmith <ssmith@7c000575-7f3e-0410-8e6b-90f0b5f38242>
2009-03-16 18:43:04 +0000

    shawn-cb-lb - creating branch

---
+++ shawn-cb-lb/src/main/java/org/codehaus/savana/scripts/DiffChangesFromSource.java
@@ -0,0 +1,135 @@
+/*
+ * Savana - Transactional Workspaces for Subversion
+ * Copyright (C) 2006-2009  Bazaarvoice Inc.
+ * <p/>
+ * This file is part of Savana.
+ * <p/>
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * <p/>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * <p/>
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Third party components of this software are provided or made available only subject
+ * to their respective licenses. The relevant components and corresponding
+ * licenses are listed in the "licenses" directory in this distribution. In any event,
+ * the disclaimer of warranty and limitation of liability provision in this Agreement
+ * will apply to all Software in this distribution.
+ *
+ * @author Brian Showers (brian@bazaarvoice.com)
+ * @author Bryon Jacob (bryon@jacob.net)
+ * @author Shawn Smith (shawn@bazaarvoice.com)
+ */
+package org.codehaus.savana.scripts;
+
+import org.codehaus.savana.FileListDiffGenerator;
+import org.codehaus.savana.MetadataProperties;
+import org.codehaus.savana.WorkingCopyInfo;
+import org.tmatesoft.svn.cli.svn.SVNOption;
+import org.tmatesoft.svn.core.SVNDepth;
+import org.tmatesoft.svn.core.SVNErrorCode;
+import org.tmatesoft.svn.core.SVNErrorMessage;
+import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.SVNProperties;
+import org.tmatesoft.svn.core.SVNURL;
+import org.tmatesoft.svn.core.internal.wc.SVNErrorManager;
+import org.tmatesoft.svn.core.wc.DefaultSVNDiffGenerator;
+import org.tmatesoft.svn.core.wc.SVNDiffClient;
+import org.tmatesoft.svn.core.wc.SVNRevision;
+import org.tmatesoft.svn.util.SVNLogType;
+
+import java.io.File;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class DiffChangesFromSource extends SAVCommand {
+
+    public DiffChangesFromSource() {
+        super("diffchangesfromsource", new String[]{"diff"});
+    }
+
+    @Override
+    protected Collection createSupportedOptions() {
+        ArrayList options = new ArrayList();
+//        options.add(SVNOption.DIFF_CMD);  // doesn't work correctly in SVNKit v1.2.2, throws NPE if changeset has an added file
+        options.add(SVNOption.EXTENSIONS);
+        options.add(SVNOption.NO_DIFF_DELETED);
+        options.add(SVNOption.FORCE);
+        return options;
+    }
+
+    public void doRun() throws SVNException {
+        SAVCommandEnvironment env = getSVNEnvironment();
+
+        //Parse command-line arguments
+        List<String> targets = env.combineTargets(null, false);
+        if (targets.size() > 0) {
+            SVNErrorManager.error(SVNErrorMessage.create(SVNErrorCode.CL_ARG_PARSING_ERROR), SVNLogType.CLIENT);
+        }
+
+        WorkingCopyInfo wcInfo = new WorkingCopyInfo(env.getClientManager());
+        MetadataProperties wcProps = wcInfo.getMetadataProperties();
+
+        //If there is no source (we are in the trunk)
+        if (wcProps.getSourceRoot() == null) {
+            String errorMessage = "Error: No source path found (you are probably in the TRUNK).";
+            SVNErrorManager.error(SVNErrorMessage.create(SVNErrorCode.ILLEGAL_TARGET, errorMessage), SVNLogType.CLIENT);
+        }
+
+        //Create the source URL
+        SVNURL sourceURL = wcInfo.getRepositoryURL(wcProps.getSourcePath());
+
+        //Diff [source:HEAD, branch:HEAD] to see what has changes
+        logStart("Get Diff Client");
+        SVNDiffClient diffClient = env.getClientManager().getDiffClient();
+        final String metadataFile = wcInfo.getMetadataFile().getAbsolutePath();
+        DefaultSVNDiffGenerator diffGenerator = new DefaultSVNDiffGenerator() {
+            @Override
+            protected String getExternalDiffCommand() {
+                return null;  // doesn't work correctly in SVNKit v1.2.2, throws NPE if changeset has an added file
+            }
+            @Override
+            public void displayPropDiff(String path, SVNProperties baseProps, SVNProperties diff, OutputStream result) throws SVNException {
+                // ignore changes in the metadata file since it will be reverted on promote
+                if (!new File(path).getAbsolutePath().equals(metadataFile)) {
+                    super.displayPropDiff(path, baseProps, FileListDiffGenerator.getInterestingProperties(diff), result);
+                }
+            }
+            @Override
+            public void displayFileDiff(String path, File file1, File file2, String rev1, String rev2, String mimeType1, String mimeType2, OutputStream result) throws SVNException {
+                // ignore changes in the metadata file since it will be reverted on promote
+                if (!new File(path).getAbsolutePath().equals(metadataFile)) {
+                    super.displayFileDiff(path, file1, file2, rev1, rev2, mimeType1, mimeType2, result);
+                }
+            }
+        };
+        if (env.getDiffCommand() != null) {
+            diffGenerator.setExternalDiffCommand(env.getDiffCommand());
+            diffGenerator.setRawDiffOptions(env.getExtensions());
+        } else {
+            diffGenerator.setDiffOptions(env.getDiffOptions());
+        }
+        diffGenerator.setDiffDeleted(!env.isNoDiffDeleted());
+        diffGenerator.setForcedBinaryDiff(env.isForce());
+        diffGenerator.setBasePath(new File("").getAbsoluteFile());
+        diffGenerator.setFallbackToAbsolutePath(true);
+        diffGenerator.setOptions(diffClient.getOptions());
+        diffClient.setDiffGenerator(diffGenerator);
+        logEnd("Get Diff Client");
+
+        logStart("Do Diff");
+        diffClient.doDiff(sourceURL, wcProps.getLastMergeRevision(), wcInfo.getRootDir(),
+                SVNRevision.WORKING, SVNDepth.INFINITY, false, env.getOut(), null);
+        logEnd("Do Diff");
+    }
+}
---
+++ shawn-cb-lb/src/main/java/org/codehaus/savana/scripts/SAVOption.java
@@ -0,0 +1,67 @@
+/*
+ * Savana - Transactional Workspaces for Subversion
+ * Copyright (C) 2009  Bazaarvoice Inc.
+ * <p/>
+ * This file is part of Savana.
+ * <p/>
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * <p/>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * <p/>
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Third party components of this software are provided or made available only subject
+ * to their respective licenses. The relevant components and corresponding
+ * licenses are listed in the "licenses" directory in this distribution. In any event,
+ * the disclaimer of warranty and limitation of liability provision in this Agreement
+ * will apply to all Software in this distribution.
+ *
+ * @author Brian Showers (brian@bazaarvoice.com)
+ * @author Bryon Jacob (bryon@jacob.net)
+ * @author Shawn Smith (shawn@bazaarvoice.com)
+ */
+package org.codehaus.savana.scripts;
+
+import org.tmatesoft.svn.cli.AbstractSVNOption;
+
+/**
+ */
+public class SAVOption extends AbstractSVNOption {
+
+    public static final SAVOption CHANGE_ROOT = new SAVOption("changeRoot");
+
+    public static final SAVOption PROJECT_NAME = new SAVOption("projectName", false);
+    public static final SAVOption TRUNK_PATH = new SAVOption("trunkPath", false);
+    public static final SAVOption RELEASE_BRANCHES_PATH = new SAVOption("releaseBranchesPath", false);
+    public static final SAVOption USER_BRANCHES_PATH = new SAVOption("userBranchesPath", false);
+    public static final SAVOption SAVANA_POLICIES_FILE = new SAVOption("savanaPoliciesFile", false);
+
+    private SAVOption(String name) {
+        this(name, null, true);
+    }
+
+    private SAVOption(String name, boolean unary) {
+        this(name, null, unary);
+    }
+
+    private SAVOption(String name, String alias) {
+        this(name, alias, true);
+    }
+
+    private SAVOption(String name, String alias, boolean unary) {
+        super(name, alias, unary);
+    }
+
+    @Override
+    protected String getResourceBundleName() {
+        return "org.codehaus.savana.scripts.options";
+    }
+}
