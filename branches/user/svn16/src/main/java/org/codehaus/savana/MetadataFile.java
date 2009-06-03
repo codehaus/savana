9f6a43e3bd0dcb8352b06444a3c1c5332d2d2879
ssmith <ssmith@7c000575-7f3e-0410-8e6b-90f0b5f38242>
2009-06-03 22:08:51 +0000

    svn16 - creating branch

---
+++ branches/user/svn16/src/main/java/org/codehaus/savana/MetadataFile.java
@@ -0,0 +1,64 @@
+/*
+ * Savana - Transactional Workspaces for Subversion
+ * Copyright (C) 2006-2009  Bazaarvoice Inc.
+ * <p/>
+ * This file is part of Savana.
+ * <p/>
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * <p/>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * <p/>
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Third party components of this software are provided or made available only subject
+ * to their respective licenses. The relevant components and corresponding
+ * licenses are listed in the "licenses" directory in this distribution. In any event,
+ * the disclaimer of warranty and limitation of liability provision in this Agreement
+ * will apply to all Software in this distribution.
+ *
+ * @author Brian Showers (brian@bazaarvoice.com)
+ * @author Bryon Jacob (bryon@jacob.net)
+ * @author Shawn Smith (shawn@bazaarvoice.com)
+ */
+package org.codehaus.savana;
+
+import java.io.File;
+
+public class MetadataFile extends File {
+    public static final String METADATA_FILE_NAME = ".savana";
+    public static final String METADATA_FILE_NAME_BACKWARD_COMPATIBLE = ".svnscripts";
+
+    public static final String PROP_PROJECT_ROOT = "PROJECT_ROOT";
+    public static final String PROP_PROJECT_NAME = "PROJECT_NAME";
+    public static final String PROP_BRANCH_PATH = "BRANCH_PATH";
+    public static final String PROP_BRANCH_TYPE = "BRANCH_TYPE";
+    public static final String PROP_SOURCE_ROOT = "SOURCE_ROOT";
+    public static final String PROP_SOURCE_ROOT_BACKWARD_COMPATIBLE = "SOURCE_PATH";
+    public static final String PROP_SOURCE_SUBPATH = "SOURCE_SUBPATH";
+    public static final String PROP_BRANCH_POINT_REVISION = "BRANCH_POINT_REVISION";
+    public static final String PROP_LAST_MERGE_REVISION = "LAST_MERGE_REVISION";
+
+    public static final String PROP_TRUNK_PATH = "TRUNK_PATH";
+    public static final String PROP_RELEASE_BRANCHES_PATH = "BRANCHES_PATH";
+    public static final String PROP_USER_BRANCHES_PATH = "USER_BRANCHES_PATH";
+    public static final String PROP_SAVANA_POLICIES = "SAVANA_POLICIES";
+
+    /** Properties that should not be merged into a user branch if they change in the source. */
+    public static final String[] PROPS_DO_NOT_SYNC = {
+            PROP_BRANCH_PATH, PROP_BRANCH_TYPE,
+            PROP_SOURCE_ROOT, PROP_SOURCE_ROOT_BACKWARD_COMPATIBLE, PROP_SOURCE_SUBPATH,
+            PROP_BRANCH_POINT_REVISION, PROP_LAST_MERGE_REVISION,
+    };
+
+    public MetadataFile(File parent, String child) {
+        super(parent, child);
+    }
+}
