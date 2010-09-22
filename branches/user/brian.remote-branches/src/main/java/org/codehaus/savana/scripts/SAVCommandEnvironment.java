ebce925160f90dc712864fdcff8c80d82e267a74
brianshowers <brianshowers@7c000575-7f3e-0410-8e6b-90f0b5f38242>
2010-09-22 15:28:38 +0000

    brian.remote-branches - creating branch

---
+++ branches/user/brian.remote-branches/src/main/java/org/codehaus/savana/scripts/SAVCommandEnvironment.java
@@ -0,0 +1,167 @@
+/*
+ * Savana - Transactional Workspaces for Subversion
+ * Copyright (C) 2009  Bazaarvoice Inc.
+ * <p/>
+ * This file is part of Savana.
+ * <p/>
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * <p/>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * <p/>
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Third party components of this software are provided or made available only subject
+ * to their respective licenses. The relevant components and corresponding
+ * licenses are listed in the "licenses" directory in this distribution. In any event,
+ * the disclaimer of warranty and limitation of liability provision in this Agreement
+ * will apply to all Software in this distribution.
+ *
+ * @author Brian Showers (brian@bazaarvoice.com)
+ * @author Bryon Jacob (bryon@jacob.net)
+ * @author Shawn Smith (shawn@bazaarvoice.com)
+ */
+package org.codehaus.savana.scripts;
+
+import org.codehaus.savana.BranchType;
+import org.tmatesoft.svn.cli.AbstractSVNOption;
+import org.tmatesoft.svn.cli.SVNCommandLine;
+import org.tmatesoft.svn.cli.SVNOptionValue;
+import org.tmatesoft.svn.cli.svn.SVNCommandEnvironment;
+import org.tmatesoft.svn.core.SVNErrorCode;
+import org.tmatesoft.svn.core.SVNErrorMessage;
+import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.internal.wc.DefaultSVNOptions;
+import org.tmatesoft.svn.core.internal.wc.SVNErrorManager;
+import org.tmatesoft.svn.core.wc.ISVNConflictHandler;
+import org.tmatesoft.svn.util.SVNLogType;
+
+import java.io.File;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.lang.reflect.Field;
+import java.util.Iterator;
+import java.util.regex.Pattern;
+
+public class SAVCommandEnvironment extends SVNCommandEnvironment {
+
+    private SVNCommandLine _commandLine;
+    private boolean _changeRoot;
+    private boolean _topLevelExplicit;
+    private String _projectName;
+    private String _trunkPath = BranchType.TRUNK.getDefaultPath();
+    private String _releaseBranchesPath = BranchType.RELEASE_BRANCH.getDefaultPath();
+    private String _userBranchesPath = BranchType.USER_BRANCH.getDefaultPath();
+    private byte[] _savanaPoliciesFileData;
+
+    public SAVCommandEnvironment(String programName, PrintStream out, PrintStream err, InputStream in) {
+        super(programName, out, err, in);
+    }
+
+    protected void initOptions(SVNCommandLine commandLine) throws SVNException {
+        super.initOptions(commandLine);
+        _commandLine = commandLine;
+    }
+
+    @Override
+    protected void initOption(SVNOptionValue optionValue) throws SVNException {
+        AbstractSVNOption option = optionValue.getOption();
+        if (option == SAVOption.CHANGE_ROOT) {
+            _changeRoot = true;
+        } else if (option == SAVOption.TOP_LEVEL) {
+            _topLevelExplicit = true;
+        } else if (option == SAVOption.PROJECT_NAME) {
+            _projectName = optionValue.getValue();
+        } else if (option == SAVOption.TRUNK_PATH) {
+            _trunkPath = optionValue.getValue();
+        } else if (option == SAVOption.RELEASE_BRANCHES_PATH) {
+            _releaseBranchesPath = optionValue.getValue();
+        } else if (option == SAVOption.USER_BRANCHES_PATH) {
+            _userBranchesPath = optionValue.getValue();
+        } else if (option == SAVOption.SAVANA_POLICIES_FILE) {
+            _savanaPoliciesFileData = readFromFile(new File(optionValue.getValue()));
+        } else {
+            super.initOption(optionValue);
+        }
+    }
+
+    public ISVNConflictHandler getConflictHandler() throws SVNException {
+        DefaultSVNOptions options = getOptions();
+        //The SVNKit DefaultSVNOptions class doesn't expose a getter for the
+        //conflict handler, so use reflection to retrieve the value. 
+        try {
+            Field field = options.getClass().getDeclaredField("myConflictResolver");
+            field.setAccessible(true);
+            return (ISVNConflictHandler) field.get(options);
+        } catch (Exception e) {
+            SVNErrorManager.error(SVNErrorMessage.create(SVNErrorCode.UNKNOWN,
+                    "Incompatible change in DefaultSVNOptions: missing myConflictResolver field",
+                    null, SVNErrorMessage.TYPE_ERROR, e), SVNLogType.CLIENT);
+            return null; // for the compiler
+        }
+    }
+
+    public void setConflictHandler(ISVNConflictHandler conflictHandler) {
+        getOptions().setConflictHandler(conflictHandler);
+    }
+
+    /** Returns the SVNCommandLine object formatted as a plausible command-line string, for debugging. */
+    public String getCommandLineString() {
+        StringBuilder buf = new StringBuilder();
+        buf.append(getProgramName());
+        buf.append(" ").append(_commandLine.getCommandName());
+        for (Iterator<SVNOptionValue> options = _commandLine.optionValues(); options.hasNext();) {
+            SVNOptionValue optionValue = options.next();
+            buf.append(" ").append(optionValue.getName());
+            if (optionValue.getValue() != null) {
+                buf.append(" ").append(quoteString(optionValue.getValue()));
+            }
+        }
+        for (Object argument : _commandLine.getArguments()) {
+            buf.append(" ").append(quoteString((String) argument));
+        }
+        return buf.toString();
+    }
+
+    private String quoteString(String string) {
+        if (Pattern.compile("[^-a-zA-Z0-9_/\\\\.:]").matcher(string).find()) {
+            string = "\"" + string + "\"";
+        }
+        return string;
+    }
+
+    public boolean isChangeRoot() {
+        return _changeRoot;
+    }
+
+    public boolean isTopLevelExplicit() {
+        return _topLevelExplicit;
+    }
+
+    public String getProjectName() {
+        return _projectName;
+    }
+
+    public String getTrunkPath() {
+        return _trunkPath;
+    }
+
+    public String getReleaseBranchesPath() {
+        return _releaseBranchesPath;
+    }
+
+    public String getUserBranchesPath() {
+        return _userBranchesPath;
+    }
+
+    public byte[] getSavanaPoliciesFileData() {
+        return _savanaPoliciesFileData;
+    }
+}
---
+++ branches/user/brian.remote-branches/src/test/java/org/codehaus/savana/scripts/NonStandardLayoutsTest.java
@@ -0,0 +1,157 @@
+package org.codehaus.savana.scripts;
+
+import org.apache.commons.io.FileUtils;
+import org.codehaus.savana.scripts.admin.CreateMetadataFile;
+import org.tmatesoft.svn.core.ISVNDirEntryHandler;
+import org.tmatesoft.svn.core.SVNDepth;
+import org.tmatesoft.svn.core.SVNDirEntry;
+import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.SVNURL;
+import org.tmatesoft.svn.core.wc.SVNRevision;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class NonStandardLayoutsTest extends AbstractSavanaScriptsTestCase {
+
+    public void testNonstandardLayout() throws Exception {
+        // this is actually the "standard" configuration...
+        nonstandardConfigurationTest("myproject", "myproject/trunk",
+                                     "trunk", "branches", "branches/user", "myproject");
+        // this assumes that there is a top-level "projects" directory that contains our project,
+        // which is otherwise standard layout
+        nonstandardConfigurationTest("myproject", "projects/myproject/trunk",
+                                     "trunk", "branches", "branches/user", "projects/myproject");
+        // this assumes a deep "projects" directory, with release branches in branches/release
+        nonstandardConfigurationTest("myproject", "user/sample/projects/myproject/trunk",
+                                     "trunk", "branches/release", "branches/user",
+                                     "user/sample/projects/myproject");
+        // this assumes that trunk is called "head", and user branches are put in "workspaces"
+        nonstandardConfigurationTest("myproject", "myproject/head",
+                                     "head", "releases", "workspaces", "myproject");
+        // this assumes that "trunk" and "branches" exist at the repo root, and projects exist
+        // as children of trunk/branches
+        nonstandardConfigurationTest("myproject", "trunk/myproject",
+                                     "trunk/myproject", "branches", "branches/user", "/");
+    }
+
+    private void nonstandardConfigurationTest(String projectName,
+                                              String branchPath,
+                                              String trunkDir,
+                                              String branchesDir,
+                                              String userBranchesDir,
+                                              String projectDir) throws Exception {
+        // create a new test repository
+        SVNURL repoUrl = TestRepoUtil.newRepository(false);
+
+        // create directories along the path to trunk
+        SVNURL trunkPath = repoUrl;
+        String[] path = branchPath.split("/");
+        for (String dir : path) {
+            trunkPath = trunkPath.appendPath(dir, true);
+            SVN.getCommitClient().doMkDir(new SVNURL[]{trunkPath}, "initial creation");
+        }
+
+        // check out a working copy of the repo
+        File wc = createTempDir("non-standard-layout-wc");
+        SVN.getUpdateClient().doCheckout(trunkPath, wc, SVNRevision.UNDEFINED, SVNRevision.HEAD, SVNDepth.INFINITY, false);
+        cd(wc);
+
+        // bootstrap the project
+        savana(CreateMetadataFile.class,
+               projectDir,
+               "TRUNK",
+               "--projectName", projectName,
+               "--releaseBranchesPath", branchesDir,
+               "--userBranchesPath", userBranchesDir,
+               "--trunkPath", trunkDir);
+
+        // add a file to the trunk
+        File textFile = new File(wc, "file.txt");
+        FileUtils.writeStringToFile(textFile, "CONTENTS");
+        SVN.getWCClient().doAdd(textFile, false, false, false, SVNDepth.EMPTY, false, false);
+        SVN.getCommitClient().doCommit(new File[]{wc}, false, "committing", null, null, false, false, SVNDepth.INFINITY);
+
+        // create a branch, and a user branch from that branch.
+        savana(CreateReleaseBranch.class, "1.0");
+        savana(CreateUserBranch.class, "user-1.0", "-m", "custom commit message");
+
+        // set the wc back to the 1.0 branch
+        savana(SetBranch.class, "1.0");
+        assertTrue(savana(ListWorkingCopyInfo.class)
+                .contains("Branch Name:           1.0"));
+
+        // edit the file in 1.0
+        FileUtils.writeStringToFile(textFile, "NEW CONTENTS FROM 1.0");
+        SVN.getCommitClient().doCommit(new File[]{wc}, false, "committing", null, null, false, false, SVNDepth.INFINITY);
+
+        // set the wc to user-1.0 and sync the changes
+        savana(SetBranch.class, "user-1.0");
+        assertTrue(savana(ListWorkingCopyInfo.class)
+                .contains("Branch Name:           user-1.0"));
+        assertEquals("CONTENTS", FileUtils.readFileToString(textFile));
+        savana(Synchronize.class);
+        assertEquals("NEW CONTENTS FROM 1.0", FileUtils.readFileToString(textFile));
+        SVN.getCommitClient().doCommit(new File[]{wc}, false,
+                "synced from release branch", null, null, false, false, SVNDepth.INFINITY);
+
+        // set the wc to the trunk and back - just to show we can
+        savana(SetBranch.class, "--changeRoot", "trunk");
+        // TODO: I'm not 100% sure this is the exact behavior we want for TRUNK
+        assertTrue(savana(ListWorkingCopyInfo.class)
+                .contains("Branch Name:           " +
+                          trunkDir.substring(trunkDir.lastIndexOf("/") + 1)));
+        savana(SetBranch.class, "--changeRoot", "user-1.0");
+        assertTrue(savana(ListWorkingCopyInfo.class)
+                .contains("Branch Name:           user-1.0"));
+
+        // get a list of everything in the repo
+        final Set<String> paths = new HashSet<String>();
+        SVN.getLogClient().doList(
+                repoUrl, SVNRevision.UNDEFINED, SVNRevision.HEAD, false, true,
+                new ISVNDirEntryHandler() {
+                    public void handleDirEntry(SVNDirEntry svnDirEntry) throws SVNException {
+                        paths.add(svnDirEntry.getRelativePath());
+                    }
+                });
+        // assert that the correct directory for trunk, 1.0, and user-1.0 exist, and that each
+        // contains .savana and file.txt
+        for (String branch : Arrays.asList(trunkDir,
+                                           branchesDir + "/1.0",
+                                           userBranchesDir + "/user-1.0")) {
+            String eachBranchPath = (projectDir + "/" + branch).replaceAll("\\/+", "/")
+                    .replaceAll("^\\/", "");
+            assertTrue(eachBranchPath, paths.contains(eachBranchPath));
+            assertTrue(eachBranchPath + "/.savana", paths.contains(eachBranchPath + "/.savana"));
+            assertTrue(eachBranchPath + "/file.txt", paths.contains(eachBranchPath + "/file.txt"));
+        }
+
+        // edit the file in user-1.0
+        FileUtils.writeStringToFile(textFile, "NEW CONTENTS FROM user-1.0");
+        SVN.getCommitClient().doCommit(new File[]{wc}, false, "committing", null, null, false, false, SVNDepth.INFINITY);
+
+        // promote the workspace
+        savana(Synchronize.class);
+
+        SVN.getUpdateClient().doUpdate(wc, SVNRevision.HEAD, SVNDepth.FILES, false, false);
+        savana(Promote.class, "-m", "1.0 - promoting my changes");
+
+        // check that we're back to 1.0, and that the changes are available to us
+        assertTrue(savana(ListWorkingCopyInfo.class)
+                .contains("Branch Name:           1.0"));
+        assertEquals("NEW CONTENTS FROM user-1.0", FileUtils.readFileToString(textFile));
+
+        // check that the user-1.0 branch is now gone
+        SVN.getLogClient().doList(
+                repoUrl, SVNRevision.UNDEFINED, SVNRevision.HEAD, false, true,
+                new ISVNDirEntryHandler() {
+                    public void handleDirEntry(SVNDirEntry svnDirEntry) throws SVNException {
+                        String path = svnDirEntry.getRelativePath();
+                        assertFalse(path.contains("user-1.0"));
+                    }
+                });
+
+    }
+}
