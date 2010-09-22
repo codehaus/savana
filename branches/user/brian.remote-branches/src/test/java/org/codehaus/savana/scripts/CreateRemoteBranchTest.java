e5ddd8ef0cbe870580bae6b8c55a49d72db21d01
brianshowers <brianshowers@7c000575-7f3e-0410-8e6b-90f0b5f38242>
2010-09-22 19:13:21 +0000

    user branch

--- branches/user/brian.remote-branches/src/test/java/org/codehaus/savana/scripts/CreateRemoteBranchTest.java
+++ branches/user/brian.remote-branches/src/test/java/org/codehaus/savana/scripts/CreateRemoteBranchTest.java
@@ -1,6 +1,7 @@
 package org.codehaus.savana.scripts;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.io.FileUtils;
+import org.tmatesoft.svn.core.SVNDepth;
 import org.tmatesoft.svn.core.SVNURL;
 
 import java.io.File;
@@ -13,6 +14,11 @@ public class CreateRemoteBranchTest extends AbstractSavanaScriptsTestCase {
         String projectName = getClass().getSimpleName().toLowerCase();
         File WC1 = TestRepoUtil.setupProjectWithWC(REPO_URL, projectName, true, true, "test-project");
         long rev = SVN.getStatusClient().doStatus(WC1, false).getRevision().getNumber();
+
+        // we should be able to create remote branches even if the working copy has edits
+        File animalsFile = new File(WC1, "src/text/animals.txt");
+        String animals = FileUtils.readFileToString(animalsFile, "UTF-8");
+        FileUtils.writeStringToFile(animalsFile, animals.replaceAll("key", "goose"));
 
         // create a remote user branch
         cd(WC1);
@@ -40,6 +46,9 @@ public class CreateRemoteBranchTest extends AbstractSavanaScriptsTestCase {
                 "Last Merge Revision:   none",
                 savana(ListWorkingCopyInfo.class));
 
+        //Revert the file modification
+        SVN.getWCClient().doRevert(new File[]{animalsFile}, SVNDepth.INFINITY, null);
+
         //Make sure we can switch to the user branch
         savana(SetBranch.class, "user1");
         assertEquals(
@@ -55,11 +64,88 @@ public class CreateRemoteBranchTest extends AbstractSavanaScriptsTestCase {
                 savana(ListWorkingCopyInfo.class));
     }
 
+    public void testCreateRemoteUserSubBranch() throws Exception {
+        // setup a test project with a working directory and import the 'test-project' files
+        String projectName = getClass().getSimpleName().toLowerCase();
+        File WC1 = TestRepoUtil.setupProjectWithWC(REPO_URL, projectName, true, true, "test-project");
+        long rev = SVN.getStatusClient().doStatus(WC1, false).getRevision().getNumber();
+
+        // we should be able to create remote branches even if the working copy has edits
+        File animalsFile = new File(WC1, "src/text/animals.txt");
+        String animals = FileUtils.readFileToString(animalsFile, "UTF-8");
+        FileUtils.writeStringToFile(animalsFile, animals.replaceAll("key", "goose"));
+
+        // create a remote user branch
+        cd(WC1);
+        assertEquals(
+                "---------------------------------------------\n" +
+                "Branch Name:           user1-src\n" +
+                "Branch Subpath:        src\n" +
+                "---------------------------------------------\n" +
+                "Project Name:          " + projectName + "\n" +
+                "Branch Type:           user branch\n" +
+                "Source:                trunk\n" +
+                "Branch Point Revision: " + rev + "\n" +
+                "Last Merge Revision:   " + rev,
+                savana(CreateUserBranch.class, "user1-src", "src", "--remote"));
+
+        //Make sure the working copy is unchanged at the root
+        assertEquals(
+                WC1 + ":\n" +
+                "---------------------------------------------\n" +
+                "Branch Name:           trunk\n" +
+                "---------------------------------------------\n" +
+                "Project Name:          " + projectName + "\n" +
+                "Branch Type:           trunk\n" +
+                "Source:                none\n" +
+                "Branch Point Revision: none\n" +
+                "Last Merge Revision:   none",
+                savana(ListWorkingCopyInfo.class));
+
+        // change into the subbranch and make sure that the working copy is unchanged
+        File WC1_src = new File(WC1, "src");
+        cd(WC1_src);
+        assertEquals(
+                WC1 + ":\n" +
+                "---------------------------------------------\n" +
+                "Branch Name:           trunk\n" +
+                "---------------------------------------------\n" +
+                "Project Name:          " + projectName + "\n" +
+                "Branch Type:           trunk\n" +
+                "Source:                none\n" +
+                "Branch Point Revision: none\n" +
+                "Last Merge Revision:   none",
+                savana(ListWorkingCopyInfo.class));
+
+        //Revert the file modification
+        SVN.getWCClient().doRevert(new File[]{animalsFile}, SVNDepth.INFINITY, null);
+
+        //Make sure we can switch to the user sub branch
+        savana(SetBranch.class, "user1-src", "--force");
+        assertEquals(
+                WC1_src + ":\n" +
+                "---------------------------------------------\n" +
+                "Branch Name:           user1-src\n" +
+                "Branch Subpath:        src\n" +
+                "---------------------------------------------\n" +
+                "Project Name:          " + projectName + "\n" +
+                "Branch Type:           user branch\n" +
+                "Source:                trunk\n" +
+                "Branch Point Revision: " + rev + "\n" +
+                "Last Merge Revision:   " + rev,
+                savana(ListWorkingCopyInfo.class));
+    }
+
     public void testCreateRemoteReleaseBranch() throws Exception {
         // setup a test project with a working directory and import the 'test-project' files
         String projectName = getClass().getSimpleName().toLowerCase();
         File WC1 = TestRepoUtil.setupProjectWithWC(REPO_URL, projectName, true, true, "test-project");
         long rev = SVN.getStatusClient().doStatus(WC1, false).getRevision().getNumber();
+
+        // we should be able to create remote branches even if the working copy has edits
+        File animalsFile = new File(WC1, "src/text/animals.txt");
+        String animals = FileUtils.readFileToString(animalsFile, "UTF-8");
+        FileUtils.writeStringToFile(animalsFile, animals.replaceAll("key", "goose"));
 
         // create a remote release branch
         cd(WC1);
@@ -87,8 +173,11 @@ public class CreateRemoteBranchTest extends AbstractSavanaScriptsTestCase {
                 "Last Merge Revision:   none",
                 savana(ListWorkingCopyInfo.class));
 
+        //Revert the file modification
+        SVN.getWCClient().doRevert(new File[]{animalsFile}, SVNDepth.INFINITY, null);
+
         //Make sure we can switch to the release branch
-        savana(SetBranch.class, "b1.1.x", "--changeRoot");
+        savana(SetBranch.class, "b1.1.x", "--force", "--changeRoot");
         assertEquals(
                 WC1 + ":\n" +
                 "---------------------------------------------\n" +
