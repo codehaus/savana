2d1c37b467c0c8e57084df3caeeea4b14e44bcce
bryon <bryon@7c000575-7f3e-0410-8e6b-90f0b5f38242>
2010-11-24 04:00:38 +0000

    additional testing

--- branches/user/SVNA-35/src/test/java/org/codehaus/savana/scripts/PromoteWithExternalsTest.java
+++ branches/user/SVNA-35/src/test/java/org/codehaus/savana/scripts/PromoteWithExternalsTest.java
@@ -2,16 +2,16 @@ package org.codehaus.savana.scripts;
 
 import org.apache.commons.io.FileUtils;
 import org.tmatesoft.svn.core.SVNDepth;
+import org.tmatesoft.svn.core.SVNException;
 import org.tmatesoft.svn.core.SVNPropertyValue;
 import org.tmatesoft.svn.core.SVNURL;
-import org.tmatesoft.svn.core.wc.SVNInfo;
-import org.tmatesoft.svn.core.wc.SVNRevision;
+import org.tmatesoft.svn.core.wc.*;
 
 import java.io.File;
 
 /**
  * Test that svn:externals tied to a specific revision don't cause promotes to fail.
- *
+ * <p/>
  * This test validates the fix for http://jira.codehaus.org/browse/SVNA-35
  */
 public class PromoteWithExternalsTest extends AbstractSavanaScriptsTestCase {
@@ -39,7 +39,7 @@ public class PromoteWithExternalsTest extends AbstractSavanaScriptsTestCase {
 
         // set the svn:externals on this repo to reference the contents of our external repo
         String externalsValue = String.format("mammals -r %s %s", rev, extUrl);
-        SVN.getWCClient().doSetProperty( wc, "svn:externals",
+        SVN.getWCClient().doSetProperty(wc, "svn:externals",
                 SVNPropertyValue.create(externalsValue), true, SVNDepth.EMPTY, null, null);
         SVN.getCommitClient().doCommit(
                 new File[]{wc}, false, "trunk commit - adding svn:externals", null, null, false,
@@ -50,7 +50,7 @@ public class PromoteWithExternalsTest extends AbstractSavanaScriptsTestCase {
         savana(CreateUserBranch.class, "user1");
 
         // make a simple change and commit it
-        FileUtils.writeStringToFile(new File(wc, "src/text/autos.txt"), "camry\nexpedition");
+        FileUtils.writeStringToFile(new File(wc, "src/text/autos.txt"), "camry\rrodeo");
         SVN.getCommitClient().doCommit(
                 new File[]{wc}, false, "user branch commit - updating autos", null, null, false,
                 false, SVNDepth.INFINITY);
@@ -58,5 +58,27 @@ public class PromoteWithExternalsTest extends AbstractSavanaScriptsTestCase {
         // verify that we can promote (this is the line that fails before making 'svn promote' use
         // a status client that ignores externals)
         savana(Promote.class, "-m", "trunk - updating autos");
+
+        // create another user branch
+        savana(CreateUserBranch.class, "user2");
+
+        // make a change to one of the files in the svn:externals directory
+        FileUtils.writeStringToFile(new File(wc, "mammals/dog/german_shephard.txt"), "bow wow wow");
+
+        // make a simple change to one of our "main" files and commit it
+        FileUtils.writeStringToFile(new File(wc, "src/text/autos.txt"), "camry\nexpedition");
+        SVN.getCommitClient().doCommit(
+                new File[]{wc}, false, "user branch commit - updating autos", null, null, false,
+                false, SVNDepth.INFINITY);
+
+        // verify that we can promote - we are leaving uncommitted changes in our "external"
+        // directory, which is okay - the modified externals directory will need to be committed
+        // separately
+        savana(Promote.class, "-m", "trunk - updating autos");
+
+        // check that the external file is modified.
+        SVNStatus svnStatus =
+                SVN.getStatusClient().doStatus(new File(wc, "mammals/dog/german_shephard.txt"), true);
+        assertEquals(SVNStatusType.STATUS_MODIFIED, svnStatus.getRemoteContentsStatus());
     }
 }
